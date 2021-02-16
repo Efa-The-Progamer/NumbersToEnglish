22e12f1b4409a90ef6e0f8b7f4aadd7d69089ee8
Esmaeil Fakheri Alamdari <esmail.f.a2000@gmail.com>
2021-02-16 16:43:34 +0330

    a bit upgrade in details

--- numberTOtext.py
+++ numberTOtext.py
@@ -1,19 +1,23 @@
-def numToText(o):
-    s = "$ One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen".split()
-    b = "Twenty Thirty Fourty Fifty Sixty Seventy Eighty Ninety".split()
+def numToText(inputnum):
+    firsts = "$ One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen".split()
+    Seconeds = "Twenty Thirty Fourty Fifty Sixty Seventy Eighty Ninety".split()
     
-    def Go(n):
-        if n < 20:
-            return s[n]
-        if n < 100:
-            return b[n//10] + " " + Go(n%10)
-        if n < 1000:
-            return s[n//100 + 1] + " Houndred " + Go(n%100)          
+    def Go(num):
+        if num < 20:
+            return firsts[num]
+        if num < 100:
+            return Seconeds[num // 10] + " " + Go(num % 10)
+        if num < 1000:
+            return firsts[num // 100 + 1] + " Houndred " + Go(num % 100)          
         for p, w in enumerate((' Thousand ', ' Million ', ' Billion '), 1):
-            if n < 1000 ** (p + 1):
-                return Go(n // 1000 ** p) + w + Go(n % 1000 ** p)
-    if o == 0:return "zero"
-    elif o < 0:return "Minus "+''.join(Go(-o))
-    else:return ''.join(Go(o))
+            if num < 1000 ** (p + 1):
+                return Go(num // 1000 ** p) + w + Go(num % 1000 ** p)
+    if inputnum == 0:
+        return "zero"
+    elif inputnum < 0:
+        return "Minus "+''.join(Go(-inputnum))
+    else:
+        return ''.join(Go(inputnum))
 
 print(numToText(-23223))
+# Minus Fourty Three Thousand Three Houndred Fourty Three
